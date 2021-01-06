e71599c5223e6bbbe7c1c1589d332cc0caf783d0
lawrence <lawrencelanzaderas020497@gmail.com>
2021-01-06 11:31:27 +0800

    eguls

--- my_test_tasks/event_registration/__manifest__.py
+++ my_test_tasks/event_registration/__manifest__.py
@@ -7,7 +7,7 @@
         subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        Long description of module's pur pose
+        Long description of module's purpose
     """,
 
     'author': "My Company",
